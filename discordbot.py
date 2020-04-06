1131c9dacec7a0629736ff466129bda9420deb17
nkmr001 <nakamoriyutao@icloud.com>
2020-04-07 00:41:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,9 +27,8 @@ async def corona(ctx):
     soup = BeautifulSoup(res, 'html.parser')
     hoge = soup.find_all('div', class_='brief-item')
     for h in soup.find_all('li', class_='brief-item__title'):
-        if len(h) <= 5:
-            await ctx.send(h[::-1].get_text())
-        elif len(h) == 6:
+        await ctx.send(h[::-1].get_text())
+        if len(h) == 5:
             break
 
 bot.run(token)
