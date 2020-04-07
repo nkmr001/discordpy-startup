a8b348a5c662f1f3912347ccd6b77597954dc9d2
nkmr001 <nakamoriyutao@icloud.com>
2020-04-07 18:20:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -57,17 +57,17 @@ async def ジャスティス(ctx):
 @bot.command()
 async def リリカ(ctx):
 	await ctx.send("リリカの情報\n\n組んで相性の良いキャラ\n・マルコス\n・アタリ\n・周囲カノーネor周囲スタン持ち\n\n有利対面のキャラ\n・自分より射程の短いガンナー\n・ソーン\n\n不利対面のキャラ\n・足の速いアタッカー\n・ギルガメッシュ\n・攻撃寄りスプリンター\n\n相性の良いカード、立ち回り等\nhttps://games.app-liv.jp/archives/230284#link05")
-	
+
 
 @bot.command()
 async def ノホ(ctx):
 	await ctx.send("ノホの情報\n\n組んで相性の良いキャラ\n・ディズィー\n・トマス\n・周囲カノーネor周囲スタン持ち\n\n有利対面のキャラ\n・体力倍率が0.85以下のキャラほぼ全て(マジスク＆ゆらら前提)\n\n不利対面のキャラ\n・ギルガメッシュ\n・グスタフ\n・きらら\n\n相性の良いカード、立ち回り等\nhttps://games.app-liv.jp/archives/232243#link05")
-	
+
 
 @bot.command()
 async def 忠臣(ctx):
 	await ctx.send("忠臣の情報\n\n組んで相性の良いキャラ\n・射程の長いガンナー\n・周囲カノーネor周囲スタン持ち\n\n有利対面のキャラ\n・体力倍率0.8以下のキャラ\n\n不利対面のキャラ\n・自分より足が速いキャラ\n\n相性の良いカード、立ち回り等\nhttps://games.app-liv.jp/archives/231602#link05")
-	
+
 
 @bot.command()
 async def ジャンヌ(ctx):
