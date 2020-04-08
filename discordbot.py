0e146213295f024999cab3440949f7ff8c2ed497
nkmr001 <nakamoriyutao@icloud.com>
2020-04-08 14:17:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,11 +12,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 pong = random.randrange(10)
 
 
-@bot.event
-async def on_message(message):
-	if message.author.bot:
-		time.sleep(5)
-		message.delete()
 
 @bot.event
 async def on_command_error(ctx, error):
