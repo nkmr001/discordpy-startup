fcad3eb92f10942b02179338fa6f8a2beda995ba
nkmr001 <nakamoriyutao@icloud.com>
2020-04-10 05:47:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,7 +60,7 @@ async def on_message(message):
 			embed = discord.Embed(title="このbotの説明書",description="ヘルプ以外のメッセージは３０秒で消えちゃうよ\nコマンド見にくくてごめん。")
 			embed.add_field(name="！コンパス",value="コンパスのキャラとステージについて解説するよ。\nキャラは相性の良いキャラと有利対面、不利対面を出します。\n対面については全てタイマンを想定しています。\n耐久型キャラクターのタイマンは倒されやすいかどうかを書いています。\n\nおすすめカードと立ち回りについては\nhttps://twitter.com/compass_AG\nの記事を引用しています。\n\nステージの立ち回りは\nhttps://twitter.com/wp_leagueの動画を引用しています。",inline=False)
 			embed.add_field(name="！バイオハザード",value="https://japan-cov-19.now.sh/\nから最新のコロナ感染者の情報を５件表示するよ",inline=False)
-			embed.add_field(name="！招待URL",value="このbotを他のサーバーに入れるためのURLが出てくるよ",inline=False))
+			embed.add_field(name="！招待URL",value="このbotを他のサーバーに入れるためのURLが出てくるよ",inline=False)
 			embed.add_field(name="作者",value="コンパスで語ってることが間違っていたり、追加して欲しい機能があれば\nhttps://twitter.com/mimiQ0012\nのDMによろしくね！！",inline=False)
 			await ctx.send(embed=embed)
 
