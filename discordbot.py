5bb085ee033d94300021af35a5d27dd371304e09
nkmr001 <nakamoriyutao@icloud.com>
2020-06-14 18:54:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,14 +15,12 @@ def check_with(ctx):
 @bot.command()
 async def スタート(ctx):
 #@commands.check(check_with)
-	while True:
-		url = "https://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0"
-		res = requests.get(url).text
-		soup = BeautifulSoup(res, 'html.parser')
-		for hoge in soup.find_all('div', class_='post-list-row'):
-			for h in soup.find_all('div', class_='title')[0:3]:
-				if "雪ミク" in h.get_text():
-					await ctx.send(h.get_text()+'\nhttps://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0')
-		time.sleep(21600)
+	url = "https://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0"
+	res = requests.get(url).text
+	soup = BeautifulSoup(res, 'html.parser')
+	for hoge in soup.find_all('div', class_='post-list-row'):
+		for h in soup.find_all('div', class_='title')[0:3]:
+			if "雪ミク" in h.get_text():
+				await ctx.send(h.get_text()+'\nhttps://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0')
 
 bot.run(token)
