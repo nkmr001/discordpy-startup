6603c5a791d41677ea34a3da8f512881345eebfc
nkmr001 <nakamoriyutao@icloud.com>
2020-06-14 19:10:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,20 +7,23 @@ bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 bot.remove_command('help')
 
-with_you = ["256433255723565057"]
+with_you = ["256433255723565057",263614623238848522]
 
 def check_with(ctx):
 	return ctx.message.author.id in with_you
 
 @bot.command()
-async def (ctx):
-#@commands.check(check_with)
-	url = "https://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0"
-	res = requests.get(url).text
-	soup = BeautifulSoup(res, 'html.parser')
-	for hoge in soup.find_all('div', class_='post-list-row'):
-		for h in soup.find_all('div', class_='title')[0:3]:
-			if "雪ミク" in h.get_text():
-				await ctx.send(h.get_text()+'\nhttps://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0')
+async def a(ctx):
+@commands.check(check_with)
+	while True:
+		url = "https://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0"
+		res = requests.get(url).text
+		soup = BeautifulSoup(res, 'html.parser')
+		for hoge in soup.find_all('div', class_='post-list-row'):
+			for h in soup.find_all('div', class_='title')[0:5]:
+				if "リゼロ" in h.get_text():
+					await ctx.send(f"{ctx.message.author.mention} "+h.get_text()+'\nhttps://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0')
+				else:await ctx.send(f"{ctx.message.author.mention} ありませんでした！")
+		time.sleep(21600)
 
 bot.run(token)
