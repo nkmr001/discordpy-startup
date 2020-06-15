860487726ad0c8981ed305164d91f69c8215b2c7
nkmr001 <nakamoriyutao@icloud.com>
2020-06-15 17:35:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,16 +7,13 @@ bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 bot.remove_command('help')
 
-@bot.command()
-async def a(ctx):
-	while True:
-		url = "https://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0"
-		res = requests.get(url).text
-		soup = BeautifulSoup(res, 'html.parser')
-		for hoge in soup.find_all('div', class_='post-list-row'):
-			for h in soup.find_all('div', class_='title')[0:5]:
-				if "リゼロ" in h.get_text():await ctx.send(f"{ctx.message.author.mention} "+h.get_text()+'\nhttps://rmt.club/post_list?title=5121&search_word=&deal_type_id=1&price_lower=&price_upper=&is_search=1&sort=update&deal_account_id=0')
-				else:await ctx.send(f"{ctx.message.author.mention} ありませんでした！")
-		time.sleep(21600)
+@bot.event
+async def on_message(message):
+	channel = message.channel
+	if "リヴァイ弱" in message.content:
+		try:
+			await channel.send("立体機動装置を見たことはあるか？")
+			await bot.kick(message.author.id)
+		except:await channel.send("作戦の本質を見失った")
 
 bot.run("token")
