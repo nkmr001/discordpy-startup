a69bd1c4c85c41e5d5f1847e348f9a46f2a787e7
nkmr001 <nakamoriyutao@icloud.com>
2020-07-31 14:45:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,6 +14,7 @@ except:pass
 
 @bot.command()
 async def check(ctx):
+	rank = {}
 	ch = bot.get_channel(714188878734688287)
 	async for message in ch.history(limit=200):
 		rea = message.reactions
@@ -27,11 +28,12 @@ async def check(ctx):
 				c+=int(i.count)
 		rank[message.content] = int(c)
 	rank2 = sorted(rank.items(), key=lambda x:x[1],reverse=True)
+	print(rank2)
 	embed = discord.Embed(title="映画",description=None,color=0xff0000)
 	co = 0
 	for em in rank2:
 		co+=1
-		embed.add_field(name=str(co)+'位 '+str(em[1])+'点',value=em[0],inline=False)
+		embed.add_field(name=str(co)+'位\n'+em[0],value=str(em[1])+'点',inline=False)
 	await ctx.send(embed=embed)
 
 @bot.command()
